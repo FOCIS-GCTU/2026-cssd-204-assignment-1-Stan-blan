75f58e52b9ba3fb844fdab3c264f50cd4720d652
Stan-blan <kwamedziweni745@gmail.com>
2026-05-30 00:42:25 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,6 +1,38 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Stanley Blankson Gyimah
+# STUDENT ID: 2425402029
+# Email: 2425402029@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Stanley Blankson Gyimah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # Print my initials in large stylized letters.
+
+    print()
+
+    print("...SBG")
+
+    print()
+
+    print("....SSSSSSSSSS........BBBBBBBBBB..........GGGGGGGGGGG....****")
+    print("...SSSSSSSSSSS........BBBBBBBBBBB.........GGGGGGGGGGGG...****")
+    print("...SSSS...............BBB......BBB........GGG............****")
+    print("...SSSS...............BBB......BBB........GGG............****")
+    print("....SSSSS.............BBBBBBBBBBB.........GGG....GGGGG...****")
+    print("........SSSS..........BBB......BBB........GGG......GGG...****")
+    print("..........SSSS........BBB......BBB........GGG......GGG...****")
+    print("..........SSSS........BBB......BBB........GGG......GGG...****")
+    print("...SSSSSSSSSSS...**...BBBBBBBBBBB....**...GGGGGGGGGGGG...****")
+    print("...SSSSSSSSS.....**...BBBBBBBBBB.....**...GGGGGGGGGGG....****")
+
+    print()
 
 
 
